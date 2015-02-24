95f8f3bdbdc3ce566ff765b548f3fe1e6b3397ad
Evan Plaice <evanplaice@gmail.com>
2015-02-23 22:06:23 -0800

    Added the grunt 'release' task

--- gruntfile.js
+++ gruntfile.js
@@ -1,14 +1,13 @@
  'use strict';
 
  module.exports = function(grunt) {
-
    grunt.initConfig({
-    pkg: grunt.file.readJSON('package.json'),
-    aws: grunt.file.readJSON("credentials.json"),
+    //pkg: grunt.file.readJSON('package.json'),
+    credentials: grunt.file.readJSON("credentials.json"),
     s3: {
       options: {
-        accessKeyId: "<%= aws.accessKeyId %>",
-        secretAccessKey: "<%= aws.secretAccessKey %>",
+        accessKeyId: "<%= credentials.aws.accessKeyId %>",
+        secretAccessKey: "<%= credentials.aws.secretAccessKey %>",
         region: "us-west-2",
         bucket: "evanplaice.com"
       },
@@ -16,10 +15,28 @@
         cwd: "",
         src: "index.html"
       }
+    },
+    release: {
+      options: {
+        file: 'package.json',
+        add: true,
+        commit: true,
+        tag: true,
+        push: true,
+        pushTags:true,
+        npm: false,
+        github: {
+          repo: 'evanplaice/evanplaice.com',
+          usernameVar: '<%= credentials.github.username %>',
+          passwordVar: '<%= credentials.github.password %>',
+        }
+      }
     }
   });
 
   grunt.loadNpmTasks("grunt-aws");
+  grunt.loadNpmTasks("grunt-release");
 
   grunt.registerTask('sync', ['s3']);
+  grunt.registerTask('bump', ['release']);
 }
