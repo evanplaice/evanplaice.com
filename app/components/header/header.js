86097aa01147ecdf195e0118463af9e82ecbc970
Evan Plaice <evanplaice@gmail.com>
2015-12-23 18:07:11 -0800

    [App] Add HeaderComponent + template

---
+++ app/components/header/header.js
@@ -0,0 +1,19 @@
+import {
+  Component,
+  View
+} from 'angular2';
+import { ROUTER_DIRECTIVES } from 'angular2/router';
+
+@Component ({
+  selector: 'header',
+  properties: ['title']
+})
+@View ({
+  templateUrl: 'app/components/header/header.html',
+  directives: ROUTER_DIRECTIVES
+})
+export class HeaderComponent {
+  constructor() {
+    console.log('header');
+  }
+}
--- app/components/main/main.js
+++ app/components/main/main.js
@@ -2,11 +2,16 @@ import {
 	Component,
 	View
 } from 'angular2';
+import { HeaderComponent } from 'app/components/header/header';
+
 
 @Component({
 	selector: 'app'
 })
 @View({
+	directives: [
+		HeaderComponent
+	],
 	templateUrl: 'app/components/main/main.html',
 })
 export class MainComponent {
