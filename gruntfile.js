862a2c01c80f5e4664d6c5d76e931184b0cd9d62
Evan Plaice <evanplaice@gmail.com>
2015-12-25 17:31:38 -0800

    [Settings] Add 'development' and 'production' environments

--- gruntfile.js
+++ gruntfile.js
@@ -13,6 +13,42 @@ module.exports = function(grunt) {
         uploadConcurrency: 5,
         downloadConcurrency: 5
       },
+      development: {
+        options: {
+          bucket: 'dev.evanplaice.com',
+          overwrite: true,
+          differential: true,
+          gzipRename: 'ext'
+        },
+        files: [
+          { expand: true, cwd: 'jspm_packages/', src: ['**'], dest: '/jspm_packages'},
+          { expand: true, cwd: 'app/', src: ['**'], dest: '/app' },
+          { expand: true, cwd: 'assets/', src: ['**'], dest: '/assets' },
+          { expand: true, cwd: 'images/', src: ['**'], dest: '/images' },
+          { expand: true, cwd: 'styles/', src: ['**'], dest: '/styles' },
+          { expand: true, src: 'index.html', dest: '/' },
+          { expand: true, src: 'config.js', dest: '/' },
+          { dest: '/', cwd: './', action: 'delete' }
+        ]
+      },
+      staging: {
+        options: {
+          bucket: 'staging.evanplaice.com',
+          overwrite: true,
+          differential: true,
+          gzipRename: 'ext'
+        },
+        files: [
+          { expand: true, cwd: 'jspm_packages/', src: ['**'], dest: '/jspm_packages'},
+          { expand: true, cwd: 'app/', src: ['**'], dest: '/app' },
+          { expand: true, cwd: 'assets/', src: ['**'], dest: '/assets' },
+          { expand: true, cwd: 'images/', src: ['**'], dest: '/images' },
+          { expand: true, cwd: 'styles/', src: ['**'], dest: '/styles' },
+          { expand: true, src: 'index.html', dest: '/' },
+          { expand: true, src: 'config.js', dest: '/' },
+          { dest: '/', cwd: './', action: 'delete' }
+        ]
+      },
       production: {
         options: {
           bucket: 'evanplaice.com',
@@ -36,5 +72,7 @@ module.exports = function(grunt) {
   // load tasks
   grunt.loadNpmTasks('grunt-aws-s3');
   // assign tasks to grunt commands
-  grunt.registerTask('sync', ['aws_s3:production']);
+  grunt.registerTask('development', ['aws_s3:development']);
+  grunt.registerTask('staging', ['aws_s3:staging']);
+  grunt.registerTask('production', ['aws_s3:production']);
 }
