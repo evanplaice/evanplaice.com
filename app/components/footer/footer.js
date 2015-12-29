19c101d771087dfba8f57cfa92fe20d679f1e57c
Evan Plaice <evanplaice@gmail.com>
2015-12-29 09:17:45 -0800

    [Style] Style the footer and footer navigation links

--- app/components/footer/footer.js
+++ app/components/footer/footer.js
@@ -2,12 +2,16 @@ import {
   Component,
   View
 } from 'angular2/core';
+import {
+  ROUTER_DIRECTIVES
+} from 'angular2/router';
 
 @Component({
   selector: 'footer'
 })
 @View({
-  templateUrl: 'app/components/footer/footer.html'
+  templateUrl: 'app/components/footer/footer.html',
+  directives: [ ROUTER_DIRECTIVES ]
 })
 export class FooterComponent {
   constructor () {
