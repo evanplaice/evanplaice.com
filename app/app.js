b1bbee6c1f58c6df25501ad9745433d8e2051ca2
Evan Plaice <evanplaice@gmail.com>
2015-12-31 13:33:44 -0800

    [Style] Move main.css loading out of the app

--- app/app.js
+++ app/app.js
@@ -1,7 +1,6 @@
 import 'reflect-metadata';
 import 'rxjs';
 import 'semantic-ui';
-import 'styles/main.css!';
 import { bootstrap } from 'angular2/platform/browser';
 import { ROUTER_PROVIDERS } from 'angular2/router';
 import { MainComponent } from './components/main/main';
