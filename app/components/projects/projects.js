eae887ff7acb3aeb227bbf0072ae1a570121b5a2
Evan Plaice <evanplaice@gmail.com>
2016-01-06 15:29:26 -0800

    [App] Implemented the ProjectsComponent and updated the template

--- app/components/projects/projects.js
+++ app/components/projects/projects.js
@@ -2,15 +2,23 @@ import {
   Component,
   View
 } from 'angular2/core';
+import {
+  MarkdownComponent
+} from 'ng2-markdown-component';
 
 @Component({
   selector: 'projects'
 })
 @View({
-  templateUrl: 'app/components/projects/projects.html'
+  templateUrl: 'app/components/projects/projects.html',
+  directives: [
+    MarkdownComponent
+  ]
 })
 export class ProjectsComponent {
   constructor () {
     console.log('projects');
+    this.src = 'content/projects/projects.md';
+    this.gitHubLink = 'http://github.com/evanplaice/evanplaice.com/blob/master/' + this.src;
   }
 }
