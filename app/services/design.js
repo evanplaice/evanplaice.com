a98fd5d0cda460ccb94e43a33616e9037eecaee0
Evan Plaice <evanplaice@gmail.com>
2016-01-10 13:11:08 -0800

    [App] Add DesignService

---
+++ app/services/design.js
@@ -0,0 +1,37 @@
+import {
+  Injectable,
+  Inject
+} from 'angular2/core';
+import {
+  Http
+} from 'angular2/http';
+import {
+  DesignModel
+} from 'app/models/design';
+
+@Injectable()
+export class DesignService {
+  designs = [];
+
+  constructor(@Inject(Http) http) {
+    this.http = http;
+    this.loadDesigns();
+  }
+
+  getDesigns() {
+    return this.designs;
+  }
+
+  loadDesigns() {
+    this.http.get('content/designs/designs.json').toPromise()
+    .then((res) => {
+      return JSON.parse(res._body);
+    })
+    .then((items) => {
+      items.map((item) => {
+        let design = new DesignModel(item);
+        this.designs.push(design);
+      });
+    });
+  }
+}
