725ef5b0682bcd82630e5debb9f2914a644155d1
Evan Plaice <evanplaice@gmail.com>
2016-01-10 13:10:12 -0800

    [App] Add HTTP_Providers to the 'app' component

--- app/app.js
+++ app/app.js
@@ -3,8 +3,10 @@ import 'rxjs';
 import 'semantic-ui';
 import { bootstrap } from 'angular2/platform/browser';
 import { ROUTER_PROVIDERS } from 'angular2/router';
+import { HTTP_PROVIDERS } from 'angular2/http';
 import { MainComponent } from './components/main/main';
 
 bootstrap(MainComponent, [
-  ROUTER_PROVIDERS
+  ROUTER_PROVIDERS,
+  HTTP_PROVIDERS,
 ]);
