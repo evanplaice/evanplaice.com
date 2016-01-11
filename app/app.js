2e8f98b4bbebdea530785014f6329d4d2e389e8b
Evan Plaice <evanplaice@gmail.com>
2016-01-10 21:38:46 -0800

    [App] Enable production mode

--- app/app.js
+++ app/app.js
@@ -1,11 +1,13 @@
 import 'reflect-metadata';
 import 'rxjs';
 import 'semantic-ui';
+import { enableProdMode } from 'angular2/core';
 import { bootstrap } from 'angular2/platform/browser';
 import { ROUTER_PROVIDERS } from 'angular2/router';
 import { HTTP_PROVIDERS } from 'angular2/http';
 import { MainComponent } from './components/main/main';
 
+enableProdMode();
 bootstrap(MainComponent, [
   ROUTER_PROVIDERS,
   HTTP_PROVIDERS
