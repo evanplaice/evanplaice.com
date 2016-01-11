86c5602ac095980b1a3e4dee3aa3eed5b2b8fd98
Evan Plaice <evanplaice@gmail.com>
2016-01-10 18:55:24 -0800

    [App] Add ProjectService

---
+++ app/services/project.js
@@ -0,0 +1,39 @@
+import {
+  Injectable,
+  Inject
+} from 'angular2/core';
+import {
+  Http
+} from 'angular2/http';
+import {
+  ProjectModel
+} from 'app/models/project';
+
+@Injectable()
+export class ProjectService {
+  designs = [];
+
+  constructor(@Inject(Http) http) {
+    this.http = http;
+    this.loadProjects();
+  }
+
+  getProjects() {
+    return this.projects;
+  }
+
+  loadProjects() {
+    this.http.get('content/projects/projects.json').toPromise()
+    .then((res) => {
+      return JSON.parse(res._body);
+    })
+    .then((items) => {
+      items.map((item) => {
+        if (!item.hidden) {
+          let project = new ProjectModel(item);
+          this.projects.push(project);
+        }
+      });
+    });
+  }
+}
