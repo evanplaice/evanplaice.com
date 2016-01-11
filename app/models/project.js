5d7e4eda9c8f97354afd5307fd240857af690f5d
Evan Plaice <evanplaice@gmail.com>
2016-01-10 18:55:09 -0800

    [App] Add ProjectModel

---
+++ app/models/project.js
@@ -0,0 +1,17 @@
+export class ProjectModel {
+  name = '';
+  title = '';
+  category = '';
+  role = ''
+  link = '';
+  description ='';
+
+  constructor(project) {
+    this.name = project.name;
+    this.title = project.title;
+    this.category = project.category;
+    this.role = project.role;
+    this.link = project.link;
+    this.description = project.description;
+  }
+}
