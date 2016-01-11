0312613837a2af6581513137812db3b8684198fa
Evan Plaice <evanplaice@gmail.com>
2016-01-10 19:47:24 -0800

    [App] Add ProjectService as an app-level provider

--- app/app.js
+++ app/app.js
@@ -6,9 +6,11 @@ import { ROUTER_PROVIDERS } from 'angular2/router';
 import { HTTP_PROVIDERS } from 'angular2/http';
 import { MainComponent } from './components/main/main';
 import { DesignService } from './services/design';
+import { ProjectService } from './services/project';
 
 bootstrap(MainComponent, [
   ROUTER_PROVIDERS,
   HTTP_PROVIDERS,
-  DesignService
+  DesignService,
+  ProjectService
 ]);
