902a62b6d3cc80532e92193e407ce17db8b10f39
Evan Plaice <evanplaice@gmail.com>
2016-01-10 18:28:28 -0800

    [App] Add the DesignComponent + Template

---
+++ app/components/design/design.js
@@ -0,0 +1,38 @@
+import {
+  Component,
+  View
+} from 'angular2/core';
+import {
+  MarkdownComponent
+} from 'ng2-markdown-component';
+
+@Component({
+  selector: 'design',
+  inputs: [
+    'design',
+    'last'
+  ]
+})
+@View({
+  templateUrl: 'app/components/design/design.html',
+  directives: [
+    MarkdownComponent
+  ]
+})
+
+export class DesignComponent {
+  constructor() {
+    console.log('design');
+  }
+
+  descLink() {
+    if (this.design.description)
+    return 'content/designs/' + this.design.name + '/' + this.design.description;
+  }
+
+  thumbLink(i) {
+    if (this.design.images[i]) {
+      return 'content/designs/' + this.design.name + '/thumbs/' + this.design.images[i];
+    }
+  }
+}
