62bda038f7377453bfbe0165fc60b3f9d5628f96
Evan Plaice <evanplaice@gmail.com>
2016-01-12 14:07:44 -0800

    [App] Add the FRESHModel
    FRESH is a JSON resume format. The FRESHModel is used on the 'vitae'
    page.

---
+++ app/models/fresh.js
@@ -0,0 +1,25 @@
+export class FRESHModel {
+  name = '';
+  meta = {};
+  info = {};
+  contact = {};
+  location = {};
+  social = {};
+  employment = {};
+  projects = {};
+  education = {};
+  service = {};
+  skills = {};
+  samples = {};
+  writing = {};
+  reading = {};
+  recognition = {};
+  references = {};
+  testimonials = {};
+  languages = {};
+  interests = {};
+
+  constructor(obj) {
+    for (var prop in obj) this[prop] = obj[prop];
+  }
+}
