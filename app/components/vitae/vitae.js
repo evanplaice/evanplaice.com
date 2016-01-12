452cbf16c5cfc8069a07a801a97d020f196e63c1
Evan Plaice <evanplaice@gmail.com>
2016-01-12 14:33:46 -0800

    [App] Update VitaeComponent to use VitaeService

--- app/components/vitae/vitae.js
+++ app/components/vitae/vitae.js
@@ -1,13 +1,19 @@
-import { Component, View } from 'angular2/core';
+import { Component, View, Inject } from 'angular2/core';
+import { VitaeService } from 'app/services/vitae';
 
 @Component({
-  selector: 'vitae'
+  selector: 'vitae',
+  injectables: [ VitaeService ]
 })
 @View({
   templateUrl: 'app/components/vitae/vitae.html'
 })
 export class VitaeComponent {
-  constructor () {
+  constructor (@Inject(VitaeService) vitaeService) {
     // console.log('vitae');
+
+    // initialize the data (ie to avoid uninitialized errors)
+    this.vitae = vitaeService.data;
+    vitaeService.vitae$.subscribe(update => this.vitae = update);
   }
 }
