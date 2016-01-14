4121041b20770f8fb288979e4006135e988f5195
Evan Plaice <evanplaice@gmail.com>
2016-01-13 16:24:10 -0800

    [App] Add DerpPipe support to VitaeComponent

--- app/components/vitae/vitae.js
+++ app/components/vitae/vitae.js
@@ -1,12 +1,14 @@
 import { Component, View, Inject } from 'angular2/core';
 import { VitaeService } from 'app/services/vitae';
+import { DerpPipe } from 'app/pipes/derp';
 
 @Component({
   selector: 'vitae',
   injectables: [ VitaeService ]
 })
 @View({
-  templateUrl: 'app/components/vitae/vitae.html'
+  templateUrl: 'app/components/vitae/vitae.html',
+  pipes: [ DerpPipe ]
 })
 export class VitaeComponent {
   constructor (@Inject(VitaeService) vitaeService) {
