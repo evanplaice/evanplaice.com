dfcadc8a422d0ce3134f3d8b7d222c852d792a0c
Evan Plaice <evanplaice@gmail.com>
2016-01-13 16:25:45 -0800

    [App] Resume.json - LocationModel

---
+++ app/models/resume/location.js
@@ -0,0 +1,15 @@
+export class LocationModel {
+  address = '';
+  postalCode = '';
+  city = '';
+  countryCode = '';
+  region = '';
+
+  constructor(location) {
+    this.address = location.address;
+    this.postalCode = location.postalCode;
+    this.city = location.city;
+    this.countryCode = location.countryCode;
+    this.region = location.region;
+  }
+}
