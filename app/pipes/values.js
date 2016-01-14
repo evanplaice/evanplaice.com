6af6e691bcb67df044745b0a2481efcbc3d5334f
Evan Plaice <evanplaice@gmail.com>
2016-01-13 16:19:11 -0800

    [App] Add ValuesPipe pipe
    Converts an object to an array of values for iteration (ex ngFor).

---
+++ app/pipes/values.js
@@ -0,0 +1,18 @@
+import { Pipe } from 'angular2/core';
+
+/*
+  # Description:
+
+  Tranlates a dictionary into an array of values.
+
+  # Usage: 
+  ```
+  <div *ng-for="#value of object | values"> </div>
+  ```
+*/
+@Pipe({ name: 'values' })
+export class ValuesPipe {
+  transform(value, args) {
+    return Object.keys(value).map(key => value[key]);
+  }
+}
