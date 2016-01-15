5dfa2bbd7fe6c980efc451cef21d1f7b296e444a
Evan Plaice <evanplaice@gmail.com>
2016-01-14 18:00:55 -0800

    [App] Fix the DesignModel to initialize without data

--- app/models/design.js
+++ app/models/design.js
@@ -7,11 +7,13 @@ export class DesignModel {
   images = [];
 
   constructor (design) {
-    this.name = design.name;
-    this.title = design.title;
-    this.link = design.link;
-    this.styleguide = design.styleguide;
-    this.description = design.description;
-    this.images = design.images;
+    if (design) {
+      this.name = design.name;
+      this.title = design.title;
+      this.link = design.link;
+      this.styleguide = design.styleguide;
+      this.description = design.description;
+      this.images = design.images;
+    }
   }
 }
