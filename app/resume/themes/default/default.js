2b3f78d5904c5028fa9309c64ea22b978d06ff27
Evan Plaice <evanplaice@gmail.com>
2016-01-18 05:50:37 -0800

    [App] Mapped resume/themes/components to TEMPLATE_DIRECTIVES

---
+++ app/resume/themes/default/default.js
@@ -0,0 +1,15 @@
+// Default template components
+import { HeaderComponent } from './components/header';
+import { DurationComponent } from './components/duration';
+import { HighlightsComponent } from './components/highlights';
+import { CurriculumComponent } from './components/curriculum.js';
+import { KeywordsComponent } from './components/keywords';
+
+export const TEMPLATE_DIRECTIVES = [
+  HeaderComponent,
+  DurationComponent,
+  HighlightsComponent,
+  CurriculumComponent,
+  KeywordsComponent
+];
+
