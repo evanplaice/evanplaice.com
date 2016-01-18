14d23a66d4c6d08c6d133c8bc397fc2f438d4386
Evan Plaice <evanplaice@gmail.com>
2016-01-18 13:23:22 -0800

    [App] Sample template partial

---
+++ app/resume/themes/default/partials/_sample.js
@@ -0,0 +1,13 @@
+import { Component, View } from 'angular2/core';
+import { TEMPLATE_DIRECTIVES, TEMPLATE_PIPES } from 'app/resume/themes/default/default';
+
+@Component({
+  selector: '_sample',
+  inputs: [ '_sample' ]
+})
+@View({
+  template:  ``,
+  directives: [ TEMPLATE_DIRECTIVES ],
+  pipes: [ TEMPLATE_PIPES ]
+})
+export class _SampleComponent {}
