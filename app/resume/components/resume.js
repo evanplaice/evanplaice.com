a84b4fcafce41db7ce7d31d2d941fa36f8e18ea2
Evan Plaice <evanplaice@gmail.com>
2016-01-18 03:42:58 -0800

    [App] Moved template subcomponents to resume/shared/componenets

--- app/resume/components/resume.js
+++ app/resume/components/resume.js
@@ -1,10 +1,10 @@
 import { Component, View, ViewEncapsulation } from 'angular2/core';
 
-import { HeaderComponent } from 'app/resume/components/header.js';
-import { DurationComponent } from 'app/resume/components/duration';
-import { HighlightsComponent } from 'app/resume/components/highlights';
-import { CurriculumComponent } from 'app/resume/components/curriculum';
-import { KeywordsComponent } from 'app/resume/components/keywords';
+import { HeaderComponent } from 'app/resume/shared/components/header.js';
+import { DurationComponent } from 'app/resume/shared/components/duration';
+import { HighlightsComponent } from 'app/resume/shared/components/highlights';
+import { CurriculumComponent } from 'app/resume/shared/components/curriculum';
+import { KeywordsComponent } from 'app/resume/shared/components/keywords';
 import { DerpPipe } from 'app/resume/pipes/derp';
 
 import 'font-awesome/css/font-awesome.min.css!css';
