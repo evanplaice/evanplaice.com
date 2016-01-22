f86d36153c0165651f242c1ae3cf9f91fd9db4b9
Evan Plaice <evanplaice@gmail.com>
2016-01-22 02:33:12 -0800

    [App] Add SpeakingModel

---
+++ app/vitae/models/speaking.js
@@ -0,0 +1,37 @@
+export class SpeakingModel {
+  title = "";
+  event = "";
+  location = "";
+  date = "";
+  highlights = [];
+
+  constructor(obj) {
+    if (obj) {
+      this.init(obj);
+    }
+  }
+
+  init(obj) {
+    if (obj.title) {
+      this.title = obj.title;
+    }
+
+    if (obj.event) {
+      this.event = obj.event;
+    }
+
+    if (obj.location) {
+      this.location = obj.location;
+    }
+
+    if (obj.date) {
+      this.date = obj.date;
+    }
+
+    if (obj.highlights) {
+      obj.highlights.forEach((highlight) => {
+        this.highlights.push(highlight);
+      });
+    }
+  }
+}
