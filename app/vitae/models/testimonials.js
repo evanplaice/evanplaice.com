aaf031d1649eac5287b49293fec2af5ef551ea91
Evan Plaice <evanplaice@gmail.com>
2016-01-22 02:34:29 -0800

    [App] Add TestimonialsModel

---
+++ app/vitae/models/testimonials.js
@@ -0,0 +1,30 @@
+export class TestimonialsModel {
+  name = "";
+  quote = "";
+  flavor = "";
+  private = false;
+
+  constructor(obj) {
+    if (obj) {
+      this.init(obj);
+    }
+  }
+
+  init(obj) {
+    if (obj.name) {
+      this.name = obj.name;
+    }
+
+    if (obj.quote) {
+      this.quote = obj.quote;
+    }
+
+    if (obj.flavor) {
+      this.flavor = obj.flavor;
+    }
+
+    if (obj.private === undefined) {
+      this.private = obj.private;
+    }
+  }
+}
