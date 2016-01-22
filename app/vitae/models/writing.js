c27ab4c5722c2d8e8c722ddc5223f2eeca6adbc4
Evan Plaice <evanplaice@gmail.com>
2016-01-22 02:32:54 -0800

    [App] Add WritingModel

---
+++ app/vitae/models/writing.js
@@ -0,0 +1,45 @@
+export class WritingModel {
+  title = "";
+  flavor = "";
+  publisher = {};
+  date = "";
+  url = "";
+  writing = "";
+
+  constructor(obj) {
+    if (obj) {
+      this.init(obj);
+    }
+  }
+
+  init(obj) {
+    if (obj.title) {
+      this.title = obj.title;
+    }
+
+    if (obj.flavor) {
+      this.flavor = obj.flavor;
+    }
+
+    if (obj.publisher) {
+      if (obj.publisher.name) {
+        this.publisher.name = obj.publisher.name;
+      }
+      if (obj.publisher.url) {
+        this.publisher.url = obj.publisher.url;
+      }
+    }
+
+    if (obj.date) {
+      this.date = obj.date;
+    }
+
+    if (obj.url) {
+      this.url = obj.url;
+    }
+
+    if (obj.writing) {
+      this.writing = obj.writing;
+    }
+  }
+}
