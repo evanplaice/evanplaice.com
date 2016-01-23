6c7c2eeb660ed84ed9b3e54d2696cbb045ebb26a
Evan Plaice <evanplaice@gmail.com>
2016-01-23 08:13:25 -0800

    [App] Add CompactComponent + template + css

---
+++ app/resume/compact/components/compact.js
@@ -0,0 +1,21 @@
+import { Component, View, ViewEncapsulation } from 'angular2/core';
+import { TEMPLATE_PIPES } from '../../shared/shared';
+import { COMPACT_DIRECTIVES } from '../compact';
+
+import 'font-awesome/css/font-awesome.min.css!css';
+
+@Component({
+  selector: 'resume',
+  inputs: [ 'resume' ]
+})
+@View({
+  templateUrl: 'app/resume/compact/components/compact.html',
+  styleUrls: [ 'app/resume/compact/components/compact.css' ],
+  directives: [ COMPACT_DIRECTIVES ],
+  encapsulation: ViewEncapsulation.Native
+})
+export class CompactComponent {
+  constructor () {
+    // console.log('resume');
+  }
+}
