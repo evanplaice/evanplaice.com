5fbbdbe8284a0eb68b114dbce3d4c8435881ef57
Evan Plaice <evanplaice@gmail.com>
2016-01-23 03:34:25 -0800

    [App] Update the default template mappings

--- app/resume/default/default.js
+++ app/resume/default/default.js
@@ -1,42 +1,44 @@
 // default directives
+import { InfoComponent } from './components/info';
 import { AboutComponent } from './components/about';
 import { EmploymentComponent } from './components/employment';
 import { ProjectsComponent } from './components/projects';
-// import { SkillsComponent } from './components/skills';
+import { SkillsComponent } from './components/skills';
 import { ServiceComponent } from './components/service';
 import { EducationComponent } from './components/education';
-// import { SocialComponent } from './components/social';
+import { SocialComponent } from './components/social';
 import { RecognitionComponent } from './components/recognition';
 import { WritingComponent } from './components/writing';
-// import { ReadingComponent } from './components/reading';
+import { ReadingComponent } from './components/reading';
 import { SpeakingComponent } from './components/speaking';
-// import { GovernanceComponent } from './components/governance';
-// import { LanguagesComponent } from './components/languages';
+import { GovernanceComponent } from './components/governance';
+import { LanguagesComponent } from './components/languages';
 import { SamplesComponent } from './components/samples';
 import { ReferencesComponent } from './components/references';
 import { TestimonialsComponent } from './components/testimonials';
-// import { InterestsComponent } from './components/interests';
-// import { ExtracurricularComponent } from './components/extracurricular';
-// import { AffiliationComponent } from './components/affiliation';
+import { InterestsComponent } from './components/interests';
+import { ExtracurricularComponent } from './components/extracurricular';
+import { AffiliationsComponent } from './components/affiliations';
 
 export const DEFAULT_DIRECTIVES = [
+  InfoComponent,
   AboutComponent,
   EmploymentComponent,
   ProjectsComponent,
-  // SkillsComponent,
+  SkillsComponent,
   ServiceComponent,
   EducationComponent,
-  // SocialComponent,
+  SocialComponent,
   RecognitionComponent,
   WritingComponent,
-  // ReadingComponent,
+  ReadingComponent,
   SpeakingComponent,
-  // GovernanceComponent,
-  // LanguagesComponent,
+  GovernanceComponent,
+  LanguagesComponent,
   SamplesComponent,
   ReferencesComponent,
-  TestimonialsComponent
-  // InterestsComponent,
-  // ExtracurricularComponent,
-  // AffiliationComponent
+  TestimonialsComponent,
+  InterestsComponent,
+  ExtracurricularComponent,
+  AffiliationsComponent
 ];
