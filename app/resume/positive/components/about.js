c266f9867333a570a157b557d5c9c395823ea068
Evan Plaice <evanplaice@gmail.com>
2016-01-23 06:31:34 -0800

    [app] Add AboutComponent to positive theme

---
+++ app/resume/positive/components/about.js
@@ -0,0 +1,22 @@
+import { Component, View } from 'angular2/core';
+import { TEMPLATE_DIRECTIVES, TEMPLATE_PIPES } from '../../shared/shared';
+
+@Component({
+  selector: 'about',
+  inputs: [ 'name', 'info', 'contact' ]
+})
+@View({
+  template: `
+  <header>
+  <h1>{{ name | nolast }} <span style="color: #C00000;">{{ name | last }}</span></h1>
+  <span class="label">{{ info.label }}</span>
+  <p id="contact">
+    <span *ngIf="contact.email" class="email"><a href="mailto:{{ contact.email }}">{{ contact.email }}</a></span>
+    <span *ngIf="contact.phone" class="phone">{{ contact.phone }}</span>
+    <span *ngIf="contact.website" class="website"><a href="{{ contact.website }}">{{ contact.website }}</a></span>
+  </p>
+  </header>
+  `,
+  pipes: [ TEMPLATE_PIPES ]
+})
+export class AboutComponent {}
