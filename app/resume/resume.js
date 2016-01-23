977fdd223eb9d8ac145efa02b0cf4303204d3ca0
Evan Plaice <evanplaice@gmail.com>
2016-01-23 08:15:02 -0800

    [App] Add CompactComponent to the resume mapping

--- app/resume/resume.js
+++ app/resume/resume.js
@@ -1,5 +1,6 @@
 // fresh resume components
 import { BasisComponent } from './basis/components/basis';
+import { CompactComponent } from './compact/components/compact';
 import { ModernComponent } from './modern/components/modern';
 import { PositiveComponent } from './positive/components/positive';
 
@@ -8,6 +9,7 @@ import { TEMPLATE_MODELS } from './shared/shared';
 
 // themes
 export const BASIS_COMPONENT = BasisComponent;
+export const COMPACT_COMPONENT = CompactComponent;
 export const MODERN_COMPONENT = ModernComponent;
 export const POSITIVE_COMPONENT = PositiveComponent;
 
