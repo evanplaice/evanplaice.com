d14d7fe87cb124e7dbc64cc50490287d15021ee6
Evan Plaice <evanplaice@gmail.com>
2016-01-23 08:14:44 -0800

    [App] Add the mapping for the compact template

---
+++ app/resume/compact/compact.js
@@ -0,0 +1,44 @@
+// compact directives
+import { AboutComponent } from '../default/components/about';
+import { InfoComponent } from './components/info';
+import { EmploymentComponent } from '../default/components/employment';
+import { ProjectsComponent } from '../default/components/projects';
+import { SkillsComponent } from '../default/components/skills';
+import { ServiceComponent } from '../default/components/service';
+import { EducationComponent } from '../default/components/education';
+import { SocialComponent } from '../default/components/social';
+import { RecognitionComponent } from '../default/components/recognition';
+import { WritingComponent } from '../default/components/writing';
+import { ReadingComponent } from '../default/components/reading';
+import { SpeakingComponent } from '../default/components/speaking';
+import { GovernanceComponent } from '../default/components/governance';
+import { LanguagesComponent } from '../default/components/languages';
+import { SamplesComponent } from '../default/components/samples';
+import { ReferencesComponent } from '../default/components/references';
+import { TestimonialsComponent } from '../default/components/testimonials';
+import { InterestsComponent } from '../default/components/interests';
+import { ExtracurricularComponent } from '../default/components/extracurricular';
+import { AffiliationsComponent } from '../default/components/affiliations';
+
+export const COMPACT_DIRECTIVES = [
+  AboutComponent,
+  InfoComponent,
+  EmploymentComponent,
+  ProjectsComponent,
+  SkillsComponent,
+  ServiceComponent,
+  EducationComponent,
+  SocialComponent,
+  RecognitionComponent,
+  WritingComponent,
+  ReadingComponent,
+  SpeakingComponent,
+  GovernanceComponent,
+  LanguagesComponent,
+  SamplesComponent,
+  ReferencesComponent,
+  TestimonialsComponent,
+  InterestsComponent,
+  ExtracurricularComponent,
+  AffiliationsComponent
+];
