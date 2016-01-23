fce543789223439ddfd477e3ebc241797b415a37
Evan Plaice <evanplaice@gmail.com>
2016-01-22 23:54:25 -0800

    [App] Correct the default SamplesComponent

--- app/resume/default/components/samples.js
+++ app/resume/default/components/samples.js
@@ -13,8 +13,8 @@ import { TEMPLATE_DIRECTIVES, TEMPLATE_PIPES } from '../../shared/shared';
     <header title="Samples"><span class="fa fa-lg fa-share"></span></header>
     <div *ngFor="#sample of samples">
       <h3 *ngIf="sample.title">
-        <a *ngIf="sample.url" href="{{ sample.url }}">{{ sample.title }}</a>
         <template [ngIf]="!sample.url">{{ role.title }}</template>
+        <template [ngIf]="sample.url"><a href="{{ sample.url }}">{{ sample.title }}</a></template>
       </h3>
       <duration [start]="sample.date"></duration>
       <p>{{ sample.summary }}</p>
