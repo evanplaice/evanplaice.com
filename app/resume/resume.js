ed21e3634870dfa8352ad6edacbb796aa14e608a
Evan Plaice <evanplaice@gmail.com>
2016-01-23 06:33:49 -0800

    [App] Update the resume mapping

--- app/resume/resume.js
+++ app/resume/resume.js
@@ -1,9 +1,15 @@
-// resume components
+// fresh resume components
+import { BasisComponent } from './basis/components/basis';
 import { ModernComponent } from './modern/components/modern';
+import { PositiveComponent } from './positive/components/positive';
+
+// fresh resume models
 import { TEMPLATE_MODELS } from './shared/shared';
 
 // themes
+export const BASIS_COMPONENT = BasisComponent;
 export const MODERN_COMPONENT = ModernComponent;
+export const POSITIVE_COMPONENT = PositiveComponent;
 
 // models
 export const FRESH_MODELS = TEMPLATE_MODELS;
