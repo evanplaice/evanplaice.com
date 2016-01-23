f37601ac72715e7e893d5c3b16f393c4e4d3a2f2
Evan Plaice <evanplaice@gmail.com>
2016-01-23 08:10:42 -0800

    [App] Minor fix to positive's AboutComponent

--- app/resume/positive/components/about.js
+++ app/resume/positive/components/about.js
@@ -8,7 +8,7 @@ import { TEMPLATE_DIRECTIVES, TEMPLATE_PIPES } from '../../shared/shared';
 @View({
   template: `
   <header>
-  <h1>{{ name | nolast }} <span style="color: #C00000;">{{ name | last }}</span></h1>
+  <h1 *ngIf="name">{{ name | nolast }} <span style="color: #C00000;">{{ name | last }}</span></h1>
   <span class="label">{{ info.label }}</span>
   <p id="contact">
     <span *ngIf="contact.email" class="email"><a href="mailto:{{ contact.email }}">{{ contact.email }}</a></span>
