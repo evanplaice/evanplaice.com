6a8642e95dd41389abee81798472e878db5d4d81
Evan Plaice <evanplaice@gmail.com>
2016-01-23 03:31:46 -0800

    [App] Add InterestsComponent

---
+++ app/resume/default/components/interests.js
@@ -0,0 +1,30 @@
+import { Component, View } from 'angular2/core';
+import { TEMPLATE_DIRECTIVES, TEMPLATE_PIPES } from '../../shared/shared';
+
+@Component({
+  selector: 'interests',
+  inputs: [ 'interests' ]
+})
+@View({
+  template: `
+  <template [ngIf]="!empty()">
+  <hr>
+  <section id="interests">
+    <header title="Interests"></header>
+    <div *ngFor="#interest of interests">
+      <h3>
+        <em>{{ interest.name }}</em>
+      </h3>
+      <keywords [keywords]="interest.keywords"></keywords>
+    </div>
+  </section>
+  </template>
+  `,
+  directives: [ TEMPLATE_DIRECTIVES ],
+  pipes: [ TEMPLATE_PIPES ]
+})
+export class InterestsComponent {
+  empty () {
+    return Object.keys(this.interests).length === 0;
+  }
+}
