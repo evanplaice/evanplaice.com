69aadd45db9834ee6959920dd8d0b401dc79b8ab
Evan Plaice <evanplaice@gmail.com>
2016-01-23 06:33:30 -0800

    [App] Add the positive component mapping

---
+++ app/resume/positive/positive.js
@@ -0,0 +1,42 @@
+// positive directives
+import { AboutComponent } from './components/about';
+import { EmploymentComponent } from '../default/components/employment';
+import { ProjectsComponent } from '../default/components/projects';
+import { SkillsComponent } from '../default/components/skills';
+import { ServiceComponent } from '../default/components/service';
+import { EducationComponent } from '../default/components/education';
+import { SocialComponent } from '../default/components/social';
+import { RecognitionComponent } from '../default/components/recognition';
+import { WritingComponent } from '../default/components/writing';
+import { ReadingComponent } from '../default/components/reading';
+import { SpeakingComponent } from '../default/components/speaking';
+import { GovernanceComponent } from '../default/components/governance';
+import { LanguagesComponent } from '../default/components/languages';
+import { SamplesComponent } from '../default/components/samples';
+import { ReferencesComponent } from '../default/components/references';
+import { TestimonialsComponent } from '../default/components/testimonials';
+import { InterestsComponent } from '../default/components/interests';
+import { ExtracurricularComponent } from '../default/components/extracurricular';
+import { AffiliationsComponent } from '../default/components/affiliations';
+
+export const POSITIVE_DIRECTIVES = [
+  AboutComponent,
+  EmploymentComponent,
+  ProjectsComponent,
+  SkillsComponent,
+  ServiceComponent,
+  EducationComponent,
+  SocialComponent,
+  RecognitionComponent,
+  WritingComponent,
+  ReadingComponent,
+  SpeakingComponent,
+  GovernanceComponent,
+  LanguagesComponent,
+  SamplesComponent,
+  ReferencesComponent,
+  TestimonialsComponent,
+  InterestsComponent,
+  ExtracurricularComponent,
+  AffiliationsComponent
+];
