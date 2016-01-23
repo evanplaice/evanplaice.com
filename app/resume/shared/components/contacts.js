b85ff9d13dce9685deb8b80b40bc636dffeb1987
Evan Plaice <evanplaice@gmail.com>
2016-01-22 23:55:38 -0800

    [App] Add a shared ContactsComponent

---
+++ app/resume/shared/components/contacts.js
@@ -0,0 +1,16 @@
+import { Component, View } from 'angular2/core';
+
+@Component({
+  selector: 'contacts',
+  inputs: [ 'contacts' ]
+})
+@View({
+  template: `
+  <template [ngIf]="contacts">
+    <ul *ngFor="#contact of contacts">
+      <li>{{ contact.flavor }} - {{ contact.value }}</li>
+    </ul>
+  </template>
+  `
+})
+export class ContactsComponent {}
