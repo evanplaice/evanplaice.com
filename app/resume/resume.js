218563c3978b208321d6e4630dc73e9660b8b850
Evan Plaice <evanplaice@gmail.com>
2016-01-26 08:24:22 -0800

    [App] Updated the Resume feature mappings to expose FreshService

--- app/resume/resume.js
+++ app/resume/resume.js
@@ -1,17 +1,20 @@
-// fresh resume components
+// -------------- FRESH themes --------------
 import { BasisComponent } from './basis/components/basis';
 import { CompactComponent } from './compact/components/compact';
 import { ModernComponent } from './modern/components/modern';
 import { PositiveComponent } from './positive/components/positive';
 
-// fresh resume models
-import { TEMPLATE_MODELS } from './shared/shared';
-
-// themes
 export const BASIS_COMPONENT = BasisComponent;
 export const COMPACT_COMPONENT = CompactComponent;
 export const MODERN_COMPONENT = ModernComponent;
 export const POSITIVE_COMPONENT = PositiveComponent;
 
-// models
+// -------------- FRESH models --------------
+import { TEMPLATE_MODELS } from './shared/shared';
+
 export const FRESH_MODELS = TEMPLATE_MODELS;
+
+// -------------- FRESH service --------------
+import { ResumeService } from './shared/services/resume';
+
+export const FreshService = ResumeService;
