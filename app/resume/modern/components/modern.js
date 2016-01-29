e03f83e8454005b2d0e2c6f1c50c374d656897b9
Evan Plaice <evanplaice@gmail.com>
2016-01-28 16:25:34 -0800

    [App] Change ModernComponent to use relative template + css import

--- app/resume/modern/components/modern.js
+++ app/resume/modern/components/modern.js
@@ -1,15 +1,17 @@
 import { Component, View, ViewEncapsulation } from 'angular2/core';
 import { MODERN_DIRECTIVES } from '../modern';
 
-import 'font-awesome/css/font-awesome.min.css!css';
+import template from './modern.html!text';
+import fontAwesome from 'font-awesome/css/font-awesome.min.css!text';
+import style from './modern.css!text';
 
 @Component({
   selector: 'resume',
   inputs: [ 'resume' ]
 })
 @View({
-  templateUrl: 'app/resume/modern/components/modern.html',
-  styleUrls: [ 'app/resume/modern/components/modern.css' ],
+  template: template,
+  styles: [ style, fontAwesome ],
   directives: [ MODERN_DIRECTIVES ],
   encapsulation: ViewEncapsulation.Native
 })
