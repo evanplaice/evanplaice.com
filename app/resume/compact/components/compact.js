5efdfc56ce4707cdd1f3e85c81cacaa0dd4596f3
Evan Plaice <evanplaice@gmail.com>
2016-01-28 16:25:10 -0800

    [App] Change CompactComponent to use relative template + css import

--- app/resume/compact/components/compact.js
+++ app/resume/compact/components/compact.js
@@ -1,15 +1,17 @@
 import { Component, View, ViewEncapsulation } from 'angular2/core';
 import { COMPACT_DIRECTIVES } from '../compact';
 
-import 'font-awesome/css/font-awesome.min.css!css';
+import template from './compact.html!text';
+import fontAwesome from 'font-awesome/css/font-awesome.min.css!text';
+import style from './compact.css!text';
 
 @Component({
   selector: 'resume',
   inputs: [ 'resume' ]
 })
 @View({
-  templateUrl: 'app/resume/compact/components/compact.html',
-  styleUrls: [ 'app/resume/compact/components/compact.css' ],
+  template: template,
+  styles: [ style, fontAwesome ],
   directives: [ COMPACT_DIRECTIVES ],
   encapsulation: ViewEncapsulation.Native
 })
