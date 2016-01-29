73273a28e1f14ee7d8dcbc561e0bbcfeafaf7122
Evan Plaice <evanplaice@gmail.com>
2016-01-28 16:25:46 -0800

    [App] Change PositiveComponent to use relative template + css import

--- app/resume/positive/components/positive.js
+++ app/resume/positive/components/positive.js
@@ -1,16 +1,18 @@
 import { Component, View, ViewEncapsulation } from 'angular2/core';
-import { TEMPLATE_PIPES } from '../../shared/shared';
 import { POSITIVE_DIRECTIVES } from '../positive';
+import { TEMPLATE_PIPES } from '../../shared/shared';
 
-import 'font-awesome/css/font-awesome.min.css!css';
+import template from './positive.html!text';
+import fontAwesome from 'font-awesome/css/font-awesome.min.css!text';
+import style from './positive.css!text';
 
 @Component({
   selector: 'resume',
   inputs: [ 'resume' ]
 })
 @View({
-  templateUrl: 'app/resume/positive/components/positive.html',
-  styleUrls: [ 'app/resume/positive/components/positive.css' ],
+  template: template,
+  styles: [ style, fontAwesome ],
   directives: [ POSITIVE_DIRECTIVES ],
   pipes: [ TEMPLATE_PIPES ],
   encapsulation: ViewEncapsulation.Native
