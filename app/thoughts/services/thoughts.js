63edf912cc04158e77d4c0a6d7d35f3890a02ad6
Evan Plaice <evanplaice@gmail.com>
2016-02-02 19:05:11 -0800

    [App] Update ThoughtsService to use the new models

--- app/thoughts/services/thoughts.js
+++ app/thoughts/services/thoughts.js
@@ -1,6 +1,7 @@
 import { Injectable, Inject } from 'angular2/core';
 import { Http } from 'angular2/http';
 import { Observable } from 'rxjs/Observable';
+import { ThoughtsModel } from '../models/thoughts';
 
 @Injectable()
 export class ThoughtsService {
@@ -15,8 +16,8 @@ export class ThoughtsService {
   loadThoughts(path) {
     if (path || this.src) {
       return this.thoughts$ = this.http.get(path || this.src)
-      .map(res => res.json())
-      .startWith({ github:'', content:'', posts:[] });
+      .map(res => new ThoughtsModel(res.json()))
+      .startWith(new ThoughtsModel());
     }
   }
 }
