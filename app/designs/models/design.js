d09a4c6b0307b334bb3fdc07479e8b22d0cee764
Evan Plaice <evanplaice@gmail.com>
2016-02-02 19:11:48 -0800

    [App] Update DesignModel to be more robust

--- app/designs/models/design.js
+++ app/designs/models/design.js
@@ -8,11 +8,32 @@ export class DesignModel {
 
   constructor (design) {
     if (design) {
+      this.init(design);
+    }
+  }
+
+  init(design) {
+    if (design.name) {
       this.name = design.name;
+    }
+
+    if (design.title) {
       this.title = design.title;
+    }
+
+    if (design.link) {
       this.link = design.link;
+    }
+
+    if (design.styleguide) {
       this.styleguide = design.styleguide;
+    }
+
+    if (design.description) {
       this.description = design.description;
+    }
+
+    if (design.images) {
       this.images = design.images;
     }
   }
