18e9c6aadc963b8bf6ff55b887da914f775da140
Evan Plaice <evanplaice@gmail.com>
2016-02-02 19:07:55 -0800

    [App] Update ProjectModel to be more robust

--- app/projects/models/project.js
+++ app/projects/models/project.js
@@ -8,11 +8,32 @@ export class ProjectModel {
 
   constructor (project) {
     if (project) {
+      this.init(project);
+    }
+  }
+
+  init(project) {
+    if (project.name) {
       this.name = project.name;
+    }
+
+    if (project.title) {
       this.title = project.title;
+    }
+
+    if (project.category) {
       this.category = project.category;
+    }
+
+    if (project.role) {
       this.role = project.role;
+    }
+
+    if (project.link) {
       this.link = project.link;
+    }
+
+    if (project.description) {
       this.description = project.description;
     }
   }
