840be4756f8da13871fe133935060c7fdc182def
Evan Plaice <evanplaice@gmail.com>
2017-12-16 00:54:49 -0700

    [App] Add BrowserModule to AppModule

--- app/app/app.module.js
+++ app/app/app.module.js
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 
+// imports
+import { BrowserModule } from '@angular/platform-browser';
+
+// directives & pipes
+
+// providers & services
+
 @NgModule({
   imports: [
+    BrowserModule,
   ],
   declarations: [
   ],
