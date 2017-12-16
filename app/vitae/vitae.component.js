c6055d234ada515d2ae3c490b71456ae224a070d
Evan Plaice <evanplaice@gmail.com>
2017-12-16 05:52:41 -0700

    [App] Fix the path to ResumeService

--- app/vitae/vitae.component.js
+++ app/vitae/vitae.component.js
@@ -1,5 +1,5 @@
 import { Component, Inject } from '@angular/core';
-import { ResumeService } from 'evanplaice/ng2-resume/resume';
+import { ResumeService } from 'ng2-resume/resume';
 
 @Component({
   selector: 'vitae',
