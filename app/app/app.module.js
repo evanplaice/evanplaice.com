a99feb9eec60ac5d796949a176321a0f62e75c57
Evan Plaice <evanplaice@gmail.com>
2017-12-16 05:54:56 -0700

    [App] Remove deprecated statements from AppModule

--- app/app/app.module.js
+++ app/app/app.module.js
@@ -12,15 +12,11 @@ import { FooterComponent } from './footer.component';
 import { MarkdownComponent } from 'ng2-markdown-component';
 import { DesignComponent } from '../designs/design.component';
 import { ProjectComponent } from '../projects/project.component';
-import { MODERN_COMPONENT } from 'evanplaice/ng2-resume/resume';
 
 // providers & services
-import { ROUTER_PROVIDERS } from '@angular/router';
-import { HTTP_PROVIDERS } from '@angular/http';
 import { ThoughtsService } from '../thoughts/thoughts.service';
 import { DesignsService } from '../designs/designs.service';
 import { ProjectsService } from '../projects/projects.service';
-import { ResumeService } from 'evanplaice/ng2-resume/resume';
 
 @NgModule({
   imports: [
@@ -35,14 +31,11 @@ import { ResumeService } from 'evanplaice/ng2-resume/resume';
     FooterComponent,
     DesignComponent,
     ProjectComponent,
-    MODERN_COMPONENT
   ],
   providers: [
-    HTTP_PROVIDERS,,
     ThoughtsService,
     DesignsService,
     ProjectsService,
-    ResumeService
   ],
   bootstrap: [ AppComponent ]
 })
