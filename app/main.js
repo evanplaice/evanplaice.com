91a52842d655e448aaebae64c279e9b2d783f90d
Evan Plaice <evanplaice@gmail.com>
2017-12-16 00:51:56 -0700

    [App] Fix main.ts bootstrap

--- app/main.js
+++ app/main.js
@@ -3,7 +3,6 @@ import 'reflect-metadata';
 import 'rxjs/Rx';
 import 'semantic-ui';
 import { enableProdMode } from '@angular/core';
-import { bootstrap } from '@angular/platform-browser-dynamic';
 import { ROUTER_PROVIDERS } from '@angular/router';
 import { HTTP_PROVIDERS } from '@angular/http';
 import { AppComponent } from './app/app.component';
@@ -11,13 +10,8 @@ import { ThoughtsService } from './thoughts/thoughts.service';
 import { DesignsService } from './designs/designs.service';
 import { ProjectsService } from './projects/projects.service';
 import { ResumeService } from 'evanplaice/ng2-resume/resume';
+import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+import { AppModule } from './app/app.module';
 
 enableProdMode();
-bootstrap(AppComponent, [
-  ROUTER_PROVIDERS,
-  HTTP_PROVIDERS,
-  ThoughtsService,
-  DesignsService,
-  ProjectsService,
-  ResumeService
-]);
+platformBrowserDynamic().bootstrapModule(AppModule);
