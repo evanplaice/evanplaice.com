5dd63eb2c8c4e0f338af6c01aaa0ec094dd7690a
Evan Plaice <evanplaice@gmail.com>
2017-12-16 00:48:52 -0700

    [App] Add AppModule

---
+++ app/app/app.module.js
@@ -0,0 +1,12 @@
+import { NgModule } from '@angular/core';
+
+@NgModule({
+  imports: [
+  ],
+  declarations: [
+  ],
+  providers: [
+  ],
+  bootstrap: [ AppComponent ]
+})
+export class AppModule { }
