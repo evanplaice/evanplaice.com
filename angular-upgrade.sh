41a81f6b106647fe902dec8e42ac2b3899951d05
Evan Plaice <evanplaice@gmail.com>
2017-12-16 03:50:40 -0700

    [App] Upgrade Angular to 2.0.0-rc.6

--- angular-upgrade.sh
+++ angular-upgrade.sh
@@ -11,13 +11,13 @@ jspm uninstall reflect-metadata
 
 jspm clean
 
-jspm install @angular/core=npm:@angular/core@2.0.0-rc.5
-jspm install @angular/compiler=npm:@angular/compiler@2.0.0-rc.5
-jspm install @angular/common=npm:@angular/common@2.0.0-rc.5
-jspm install @angular/platform-browser=npm:@angular/platform-browser@2.0.0-rc.5
-jspm install @angular/platform-browser-dynamic=npm:@angular/platform-browser-dynamic@2.0.0-rc.5
-jspm install @angular/http=npm:@angular/http@2.0.0-rc.5
-jspm install @angular/router=npm:@angular/router@2.0.0-rc.2
+jspm install @angular/core=npm:@angular/core@2.0.0-rc.6
+jspm install @angular/compiler=npm:@angular/compiler@2.0.0-rc.6
+jspm install @angular/common=npm:@angular/common@2.0.0-rc.6
+jspm install @angular/platform-browser=npm:@angular/platform-browser@2.0.0-rc.6
+jspm install @angular/platform-browser-dynamic=npm:@angular/platform-browser-dynamic@2.0.0-rc.6
+jspm install @angular/http=npm:@angular/http@2.0.0-rc.6
+jspm install @angular/router=npm:@angular/router@3.0.0-rc.2
 jspm install rxjs=npm:rxjs@5.0.1
 jspm install zone.js=npm:zone.js@0.6.26
 jspm install reflect-metadata=npm:reflect-metadata@0.1.2
