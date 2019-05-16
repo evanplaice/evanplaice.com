b3ed2e5c36c5332464a0cf10ee9520425020288e
Evan Plaice <evanplaice@gmail.com>
2019-05-16 01:29:34 -0600

    Fix title

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'evanplaice'`, () => {
+  it(`should have as title 'Thoughts and Works by Evan Plaice'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('evanplaice');
+    expect(app.title).toEqual('Thoughts and Works by Evan Plaice');
   });
 
   it('should render title in a h1 tag', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('Welcome to evanplaice!');
+    expect(compiled.querySelector('h1').textContent).toContain('Thoughts and Works by Evan Plaice');
   });
 });
