fb6b0c853030ab1feceea67f8d354235d9027cc6
Evan Plaice <evanplaice@gmail.com>
2019-05-16 01:30:28 -0600

    Install fomantic-ui

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,14 @@
+import 'fomantic-ui/dist/components/reset.min.css';
+import 'fomantic-ui/dist/components/container.min.css';
+import 'fomantic-ui/dist/components/divider.min.css';
+import 'fomantic-ui/dist/components/icon.min.css';
+import 'fomantic-ui/dist/components/image.min.css';
+import 'fomantic-ui/dist/components/item.min.css';
+import 'fomantic-ui/dist/components/menu.min.css';
+import 'fomantic-ui/dist/components/grid.min.css';
+import 'fomantic-ui/dist/components/segment.min.css';
+import 'fomantic-ui/dist/components/site.min.css';
+
 import { enableProdMode } from '@angular/core';
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
