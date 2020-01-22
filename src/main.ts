a066219fde9e37751bff7f0cc61718166f75efbb
Evan Plaice <evanplaice@gmail.com>
2020-01-22 01:37:40 -0700

    Update 'wc-resume'

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,7 @@ import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
 
 import '@vanillawc/wc-markdown/index.js';
+import '@vanillawc/wc-resume/index.js';
 
 if (environment.production) {
   enableProdMode();
