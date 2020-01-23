1b03c6e7b49efc203e44a5574ad893307bcd903b
Evan Plaice <evanplaice@gmail.com>
2020-01-22 18:11:15 -0700

    Always use enableProdMode

--- src/main.ts
+++ src/main.ts
@@ -1,15 +1,10 @@
 import { enableProdMode } from '@angular/core';
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
-
 import { AppModule } from './app/app.module';
-import { environment } from './environments/environment';
 
 import '@vanillawc/wc-markdown/index.js';
 import '@vanillawc/wc-resume/index.js';
 
-if (environment.production) {
-  enableProdMode();
-}
-
+enableProdMode();
 platformBrowserDynamic().bootstrapModule(AppModule)
   .catch(err => console.error(err));
