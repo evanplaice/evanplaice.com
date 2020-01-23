f80eab6af303c7095dfc879b0ff038afaa9f9c1f
Evan Plaice <evanplaice@gmail.com>
2020-01-22 20:23:43 -0700

    Fix Compiler Errors

--- src/designs/design.component.ts
+++ src/designs/design.component.ts
@@ -7,6 +7,7 @@ import { Component } from '@angular/core';
 })
 export class DesignComponent {
   design;
+  last;
 
   constructor () {}
 
--- src/projects/project.component.ts
+++ src/projects/project.component.ts
@@ -16,6 +16,7 @@ import { Component } from '@angular/core';
 })
 export class ProjectComponent {
   project;
+  last;
 
   constructor () {}
 
