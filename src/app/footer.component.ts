c196a7c8dbe7d16cb1b209921583beea308fd488
Evan Plaice <evanplaice@gmail.com>
2020-01-25 16:35:51 -0700

    Refactor Footer

--- src/app/footer.component.ts
+++ src/app/footer.component.ts
@@ -5,9 +5,26 @@ import { Component } from '@angular/core';
   templateUrl: './footer.component.html',
   styles: [`
     nav {
+      display: flex;
+      justify-content: center;
       font-size: 1.3rem;
       font-weight: bolder;
       margin-top: 10px;
+      margin-bottom: 10px;
+    }
+
+    span {
+      flex-direction: row;
+    }
+
+    span::after {
+      content: '|';
+      margin-left: 5px;
+      margin-right: 5px;
+    }
+
+    span:last-child::after {
+      content: none;
     }
   `]
 })
