74d905680e4bfe7563d752ae7311991ce5df8ec2
Evan Plaice <evanplaice@gmail.com>
2020-01-24 20:13:11 -0700

    Fix 'Insufficient Contrast'

--- src/app/footer.component.ts
+++ src/app/footer.component.ts
@@ -3,6 +3,11 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-footer',
   templateUrl: './footer.component.html',
+  styles: [`
+    a {
+      font-weight: bolder;
+    }
+  `]
 })
 export class FooterComponent {
   constructor() {}
