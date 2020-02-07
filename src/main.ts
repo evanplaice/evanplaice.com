1208681ad28a821671c46b10febafeba27da5d4f
Evan Plaice <evanplaice@gmail.com>
2020-02-07 12:00:12 -0700

    Move regenerator-runtime to Polyfills

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,6 @@ import { enableProdMode } from '@angular/core';
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 import { AppModule } from './app/app.module';
 
-import 'regenerator-runtime';
 import '@vanillawc/wc-icon-rule/index.js';
 import '@vanillawc/wc-markdown/index.js';
 import '@vanillawc/wc-resume/index.js';
